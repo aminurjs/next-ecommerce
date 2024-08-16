36b381de7ab0ff09b332861650b2c474fd979fda
Aminur Rahman <dev.aminur@gmail.com>
2024-08-17 01:59:15 +0600

    resolved

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -6,11 +6,11 @@ import { notFound } from "next/navigation";
 const SinglePage = async ({ params }: { params: { slug: string } }) => {
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
-      {/* IMG */}
+      {/* IMG
       <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
         <ProductImages items={product.media?.items} />
       </div>
-      {/* TEXTS */}
+      {/* TEXTS 
       <div className="w-full lg:w-1/2 flex flex-col gap-6">
         <h1 className="text-4xl font-medium">{product.name}</h1>
         <p className="text-gray-500">{product.description}</p>
@@ -49,10 +49,10 @@ const SinglePage = async ({ params }: { params: { slug: string } }) => {
           </div>
         ))}
         <div className="h-[2px] bg-gray-100" />
-        {/* REVIEWS */}
+        {/* REVIEWS 
         <h1 className="text-2xl">User Reviews</h1>
         <Reviews productId={product._id!} />
-      </div>
+      </div> */}
     </div>
   );
 };
