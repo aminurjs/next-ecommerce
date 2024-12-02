ad3096fbb14badc413f743328cc92ecba6ad6394
Aminur Rahman <dev.aminur@gmail.com>
2024-12-02 21:00:33 +0600

    Added shop link

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -10,7 +10,7 @@ const slides = [
     title: "Summer Sale Collections",
     description: "Sale! Up to 50% off!",
     img: "https://images.pexels.com/photos/4482900/pexels-photo-4482900.jpeg?auto=compress&cs=tinysrgb&w=800",
-    url: "/",
+    url: "/shop",
     bg: "bg-gradient-to-r from-yellow-50 to-pink-50",
   },
   {
@@ -18,7 +18,7 @@ const slides = [
     title: "Winter Sale Collections",
     description: "Sale! Up to 50% off!",
     img: "https://images.pexels.com/photos/5935738/pexels-photo-5935738.jpeg?auto=compress&cs=tinysrgb&w=800",
-    url: "/",
+    url: "/shop",
     bg: "bg-gradient-to-r from-pink-50 to-blue-50",
   },
   {
@@ -26,7 +26,7 @@ const slides = [
     title: "Spring Sale Collections",
     description: "Sale! Up to 50% off!",
     img: "https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg?auto=compress&cs=tinysrgb&w=800",
-    url: "/",
+    url: "/shop",
     bg: "bg-gradient-to-r from-blue-50 to-yellow-50",
   },
 ];
@@ -47,27 +47,15 @@ const Slider = () => {
           >
             {/* TEXT CONTAINER */}
             <div className="h-1/2 xl:w-1/2 xl:h-full flex flex-col items-center justify-center gap-8 2xl:gap-12 text-center">
-              <h2 className="text-xl lg:text-3xl 2xl:text-5xl">
-                {slide.description}
-              </h2>
-              <h1 className="text-5xl lg:text-6xl 2xl:text-8xl font-semibold">
-                {slide.title}
-              </h1>
+              <h2 className="text-xl lg:text-3xl 2xl:text-5xl">{slide.description}</h2>
+              <h1 className="text-5xl lg:text-6xl 2xl:text-8xl font-semibold">{slide.title}</h1>
               <Link href={slide.url}>
-                <button className="rounded-md bg-black text-white py-3 px-4 ">
-                  SHOP NOW
-                </button>
+                <button className="rounded-md bg-black text-white py-3 px-4 ">SHOP NOW</button>
               </Link>
             </div>
             {/* IMAGE CONTAINER */}
             <div className="h-1/2 xl:w-1/2 xl:h-full relative">
-              <Image
-                src={slide.img}
-                alt=""
-                fill
-                sizes="100%"
-                className="object-cover"
-              />
+              <Image src={slide.img} alt="" fill sizes="100%" className="object-cover" />
             </div>
           </div>
         ))}
@@ -81,9 +69,7 @@ const Slider = () => {
             key={slide.id}
             onClick={() => setCurrent(index)}
           >
-            {current === index && (
-              <div className="w-[6px] h-[6px] bg-gray-600 rounded-full"></div>
-            )}
+            {current === index && <div className="w-[6px] h-[6px] bg-gray-600 rounded-full"></div>}
           </div>
         ))}
       </div>
